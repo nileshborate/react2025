96fd2f2a5f895ab009a5a8fb37db6c8ac78d5a54
nileshborate <nileshhanumantborate@gmail.com>
2025-07-14 22:36:45 +0530

    6.digital clock question

---
+++ 6.Digital_Clock/src/App.js
@@ -0,0 +1,17 @@
+function App() {
+  let name = 'Nilesh';
+  return (
+    <>
+      <h1>Welcome React {name}</h1>
+      <h2>Hi</h2>
+    </>
+  );
+}
+
+/*
+1. When you click on Start button then show current date and time
+    10:35:10 PM
+2. Make it live.
+
+*/
+export default App;
