7d9db8bbd331480982aeba5c4a6eebbbbe338913
nileshborate <nileshhanumantborate@gmail.com>
2025-07-14 22:32:37 +0530

    useState description in commnet

--- 5.Hooks_useState/src/App.js
+++ 5.Hooks_useState/src/App.js
@@ -1,6 +1,23 @@
 import { useState } from 'react';
 import './App.css';
 function App() {
+  /*
+useState is an array.
+It takes 2 parameters
+	First parameter : current data
+	Second parameter : updated data
+  useState function parameter is default initial value.
+	i.e. default initial value indirectly assign to the first parameter
+
+  Each time you call setCounter, React re-renders the component with the new counter.
+
+
+  Things to Remember (For all hooks):
+    useState is only usable inside a React function component or a custom Hook.
+    Don't call it inside loops, conditions, or nested functions.
+    Updates are asynchronous – they don't happen instantly.
+*/
+
   let [counter, setCounter] = useState(20);
 
   const buttonEvent = () => {
