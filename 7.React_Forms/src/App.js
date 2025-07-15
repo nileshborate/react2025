8728e87322ad3cfd7c980825073d9cc5149c7806
nileshborate <nileshhanumantborate@gmail.com>
2025-07-15 22:28:57 +0530

    7.Form - Two textbox

--- 7.React_Forms/src/App.js
+++ 7.React_Forms/src/App.js
@@ -1,33 +1,56 @@
 import { useState } from 'react';
 import './App.css';
 function App() {
-  const [name, setName] = useState('');
+  const [fname, setFName] = useState('');
+  const [lname, setLName] = useState('');
   const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
 
-  const InputEvent = (event) => {
-    console.log('Event call :', event.target.name, event.target.value);
-    setName(event.target.value);
+  const InputEvent1 = (event) => {
+    setFName(event.target.value);
+  };
+  const InputEvent2 = (event) => {
+    setLName(event.target.value);
   };
 
   const showResult = (event) => {
     event.preventDefault();
-    setFirstName(name);
+    setFirstName(fname);
+    setLastName(lname);
+  };
+
+  const clearText = () => {
+    setFName('');
+    setLName('');
+    setFirstName('');
+    setLastName('');
   };
   return (
     <>
       <form onSubmit={showResult}>
         <div>
-          <h1>Welcome {firstName}</h1>
+          <h1>
+            Welcome {firstName} {lastName}
+          </h1>
           <input
             type="text"
-            placeholder="Enter Your Name"
-            onChange={InputEvent}
+            placeholder="Enter First Name"
+            onChange={InputEvent1}
             name="firstName"
+          />
+          <br />
+          <input
+            type="text"
+            placeholder="Enter Last Name"
+            onChange={InputEvent2}
+            name="lastName"
           />
           <br />
           <br />
           <button type="submit" /*onClick={showResult}*/>Click Me</button>
-          <button type="reset">Clear Text</button>
+          <button type="reset" onClick={clearText}>
+            Clear Text
+          </button>
         </div>
       </form>
     </>
