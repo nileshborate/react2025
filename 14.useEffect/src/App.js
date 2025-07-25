bc0e2759c46c5594652ad1375dfc6f75b13c0ad1
nileshborate <nileshhanumantborate@gmail.com>
2025-07-25 22:20:57 +0530

    14.useEffect

---
+++ 14.useEffect/src/App.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [num1, setNum1] = useState(0);
+  const [num2, setNum2] = useState(0);
+
+  const btnEvent1 = () => {
+    setNum1(num1 + 1);
+  };
+
+  const btnEvent2 = () => {
+    setNum2(num2 + 1);
+  };
+
+  useEffect(() => {
+    console.log('useEffect');
+  });
+  return (
+    <>
+      <button onClick={btnEvent1}>Click Me {num1}</button>
+      <button onClick={btnEvent2}>Click Me {num2}</button>
+    </>
+  );
+}
+
+export default App;
