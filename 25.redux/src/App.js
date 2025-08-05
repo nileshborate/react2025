b1921d55bd03f11cd6ba1ae26bd8725b7c2df4ab
nileshborate <nileshhanumantborate@gmail.com>
2025-08-05 22:19:33 +0530

    25. redux using slice

---
+++ 25.redux/src/App.js
@@ -0,0 +1,17 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { decrement, increment, reset } from './redux/counterSlice';
+
+function App() {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+  return (
+    <>
+      <h2>Count : {count}</h2>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
+      <button onClick={() => dispatch(reset())}>Reset</button>
+    </>
+  );
+}
+
+export default App;
