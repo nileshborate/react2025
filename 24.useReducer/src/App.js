0a303db8c6f3f7973f8a362c96bc5ceed01c757c
nileshborate <nileshhanumantborate@gmail.com>
2025-08-05 21:48:19 +0530

    24.useReducer

---
+++ 24.useReducer/src/App.js
@@ -0,0 +1,28 @@
+import { useReducer, useState } from 'react';
+
+const initialState = { count: 0 };
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return { count: state.count + 1 };
+    case 'DECREMENT':
+      return { count: state.count - 1 };
+    default:
+      return state;
+  }
+};
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <>
+      <p>Count = {state.count}</p>
+      <button onClick={() => dispatch({ type: 'INCREMENT' })}>Increment</button>
+      <button onClick={() => dispatch({ type: 'DECREMENT' })}>Decrement</button>
+    </>
+  );
+}
+
+export default App;
